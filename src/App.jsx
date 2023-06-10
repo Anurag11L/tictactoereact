48405f48cd0d7a49b1db4b1f5d1b52a46ba44706
Anurag11L <anuraglade@gmail.com>
2023-06-10 13:04:23 +0530

    finish

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Board from './gameee/Board';
+
+function App() {
+  
+  return (
+    
+    <div className='App'>
+      {/* <h1>hii</h1> */}
+      <Board/>
+    </div>
+  )
+}
+
+export default App;
